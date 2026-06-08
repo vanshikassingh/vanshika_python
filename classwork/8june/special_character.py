5434a4777f1a269f6d3dc122466c8b76d335537e
Vanshika Singh <vanshikabgy@gmail.com>
2026-06-08 15:47:29 +0530

    Update special_character.py

--- classwork/8june/special_character.py
+++ classwork/8june/special_character.py
@@ -1,7 +1,7 @@
 #wap to input a sentence from user and count 
 # the no of special char present in sentence 
 
-sentence=str(input("\nenetr teh string\n"))
+sentence=str(input("\n Enter the string\n"))
 count =0
 
 for ch in sentence:
